22d0ca2c92d062b4856af08fcabc956151cc786f
jschang0215 <jschang0215@naver.com>
2021-08-07 01:27:06 +0900

    Use C Code

--- 02-Protected-Mode/src/kernel.asm
+++ 02-Protected-Mode/src/kernel.asm
@@ -1,5 +1,7 @@
 [BITS 32]
 global _start ; Label이 public하게 사용될 수 있도록
+extern kernel_main
+
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
 
@@ -17,6 +19,8 @@ _start:
     in al, 0x92
     or al, 2
     out 0x92, al
+
+    call kernel_main
 
     jmp $
 
